02ca6d55ed6e475b2d57c40ac8e624957609cbfe
Gene <gene.ang92@gmail.com>
2015-12-27 18:39:04 +0000

    first

--- server.js
+++ server.js
@@ -1,26 +1,194 @@
-'use strict';
-
 var express = require('express');
-var mongo = require('mongodb');
-var routes = require('./app/routes/index.js');
+var http = require('http');
+var path = require('path');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+var mongoose = require('mongoose');
 
+//Yelp API setup
+var Yelp = require('yelp');
+var yelp = new Yelp({
+    consumer_key: 'DjfBzsjOO-2An8vwm2rt5w',
+    consumer_secret: 'MT5uJsUr4wRL0M9cSM8grHaPipo',
+    token: 'HExyIn5wsNfcfKK76y6qgQPKXU_Y8EIL',
+    token_secret: 'NHASOJ4wl2Wx6hBN8JsyXdphKRA'
+});
+
+//==================================================================
+// Define the strategy to be used by PassportJS
+require('dotenv').load();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var YelpSchema = new Schema({
+	id: String,
+	users: Array
+});
+var UserSchema = new Schema({
+	github: {
+		id: String,
+		displayName: String,
+		username: String,
+    publicRepos: Number
+	},
+	attending: Array
+});
+
+mongoose.model('Business', YelpSchema);
+mongoose.model('User',UserSchema);
+
+var Bar = mongoose.model('Business');
+var User = mongoose.model('User');
+var configAuth={'githubAuth': {
+		'clientID': process.env.GITHUB_KEY,
+		'clientSecret': process.env.GITHUB_SECRET,
+		'callbackURL': process.env.APP_URL + 'auth/github/callback'
+}};
+
+	passport.serializeUser(function (user, done) {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser(function (id, done) {
+		User.findById(id, function (err, user) {
+			done(err, user);
+		});
+	});
+
+	passport.use(new GitHubStrategy({
+		clientID: configAuth.githubAuth.clientID,
+		clientSecret: configAuth.githubAuth.clientSecret,
+		callbackURL: configAuth.githubAuth.callbackURL
+	},
+	function (token, refreshToken, profile, done) {
+		process.nextTick(function () {
+			User.findOne({'github.id': profile.id}, function (err, user) {
+				if (err) {
+					return done(err);
+				}
+
+				if (user) {
+					return done(null, user);
+				} else {
+					var newUser = new User();
+
+					newUser.github.id = profile.id;
+					newUser.github.username = profile.username;
+					newUser.github.displayName = profile.displayName;
+					newUser.github.publicRepos = profile._json.public_repos;
+					newUser.nbrClicks.clicks = 0;
+
+					newUser.save(function (err) {
+						if (err) {
+							throw err;
+						}
+
+						return done(null, newUser);
+					});
+				}
+			});
+		});
+	}));
+
+// Define a middleware function to be used for every secured routes
+var auth = function(req, res, next){
+  if (!req.isAuthenticated()) 
+  	res.send(401);
+  else
+  	next();
+};
+//==================================================================
+
+// Start express application
 var app = express();
 
-mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
 
-   if (err) {
-      throw new Error('Database failed to connect!');
-   } else {
-      console.log('Successfully connected to MongoDB on port 27017.');
-   }
+// Connect to mongoose
+mongoose.connect(process.env.MONGO_URI);
 
-   app.use('/public', express.static(process.cwd() + '/public'));
-   app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.cookieParser()); 
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'securedsession' }));
+app.use(passport.initialize()); // Add passport initialization
+app.use(passport.session());    // Add passport initialization
+app.use(app.router);
+app.use(express.static(__dirname + "/public"));
 
-   routes(app, db);
+//==================================================================
+// routes
+app.get('/users', auth, function(req, res){
+  //res.send([{name: "user1"}, {name: "user2"}]);
+});
 
-   app.listen(3000, function () {
-      console.log('Node.js listening on port 3000...');
-   });
+// yelp routes
+app.get('/app/yelp/:location', function(req,res){
+  yelp.search({term: 'nightlife', location: req.params.location}, function(err,data){
+    if(err) throw err;
+    (data.businesses).forEach(function(each){
+    	Bar.findOne({id : each.id}, function(err,obj){
+    		if(err)
+    			throw err;
+    		if(obj !== null){
+    			each.userArray = obj.users;	
+    		}
+    		else{
+    			each.userArray = [];
+    		}
+    	});
+    });
+    setTimeout(function(){res.json(data.businesses)},1000);
+  });
+});
 
+app.post('/app/yelp/:bar',auth,function(req,res){
+	var attended = new Bar();
+	attended.id = req.params.bar;
+	attended.users = req.body.users;
+	attended.save(function(err) {
+        if (err) throw err;
+        res.json(attended);
+    });
+});
+
+app.put('/app/yelp/:bar', auth, function(req,res){
+	Bar.update({id:req.params.bar}, {$set: {users: req.body.users}},
+	function(err) {
+    if (err) throw err;
+  });
+});
+
+//==================================================================
+
+//==================================================================
+// route to test if the user is logged in or not
+app.get('/loggedin', function(req, res) {
+  res.send(req.isAuthenticated() ? req.user : '0');
+});
+
+// route to log in
+app.post('/login', passport.authenticate('github'), function(req, res) {
+  res.json(req.user);
+});
+
+// route to log out
+app.post('/logout', function(req, res){
+  req.logOut();
+  res.send(200);
+});
+
+app.get('/auth/github', passport.authenticate('github'));
+
+	app.get('/auth/github/callback', passport.authenticate('github', {
+			successRedirect: '/',
+			failureRedirect: '/login'
+		}));
+
+//==================================================================
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
